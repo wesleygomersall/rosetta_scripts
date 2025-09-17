0ea641420aad14c4bf7bd303ee76cc4df8bbe83e
Wesley Gomersall <gomersallw@gmail.com>
2025-09-17 11:01:58 -0700

    renamed

---
+++ translate_ddG.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3 
+
+from pyrosetta import *
+
+# import pose 
+# relax pose
+# score delta G 
+# find vector between their centers of mass
+# translate along that vector
+# re-relax -maybe
+# score delta G 
+# calc delta delta G 
