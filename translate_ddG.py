cdc9c35362dea3ce23a45675c6274dfd672b5754
Wesley Gomersall <gomersallw@gmail.com>
2025-09-17 14:10:48 -0700

    output relaxed pose

--- translate_ddG.py
+++ translate_ddG.py
@@ -1,12 +1,45 @@
 #!/usr/bin/env python3 
 
-from pyrosetta import *
+import argparse
+from pyrosetta import init
+from pyrosetta import pose_from_pdb
+from pyrosetta.rosetta.protocols.relax import ClassicRelax
 
-# import pose 
-# relax pose
-# score delta G 
-# find vector between their centers of mass
-# translate along that vector
-# re-relax -maybe
-# score delta G 
-# calc delta delta G 
+def main(): 
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--input", "-i", type=str, help="Path to input pdb file.")
+    args = parser.parse_args()
+
+    scorefxn = 'ref2015'
+    relaxed_out_path = "relaxed.pdb"
+    # translated_out_path = "translated.pdb" 
+
+    init("-mute all")
+
+    pose = pose_from_pdb(args.input)
+    
+    relax = ClassicRelax() 
+    relax.set_scorefxn(scorefxn) 
+    relax.apply(pose) 
+
+    # TODO 
+    # score delta G 
+
+    pose.dump_pdb(relaxed_out_path)
+
+    # TODO 
+    # translate peptide from translated pose
+        # first find vector between two chains' centers of mass
+        # translate a chain relative to another chain  from PyRosetta manual:
+            # trans_mover = RigidBodyTransMover( pose, jump_num )
+            # trans_mover.step_size(50)
+            # trans_mover.apply( pose )
+        # relax -maybe
+        # Export pdb from translated pose 
+        # score delta G 
+
+    # TODO 
+    # calc delta delta G 
+
+if __name__ == "__main__": 
+    main()
