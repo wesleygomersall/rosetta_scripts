fee3b28fe6b2dc848e897f468cf89a85c7853dbd
wesleygomersall <gomersallw@gmail.com>
2025-09-22 15:58:02 -0700

    begin deltaG slurm jobs with file list

---
+++ run_slurm/run_deltaGs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script initiates jobs with `deltaG_complex.slurm` for each input pdb file
+# in `dGcomplex_inputs.txt`
+
+FILE_LIST="dGcomplex_inputs.txt"
+if ! [ -s "$FILE_LIST" ]; then
+    echo "$FILE_LIST is empty. Exiting."
+    exit
+fi
+
+SLURM_SCRIPT="$HOME/rosetta_scripts/run_slurm/deltaG_complex.slurm"
+
+for file in $(cat $FILE_LIST); do
+    sbatch $SLURM_SCRIPT $file
+done
+
+exit
