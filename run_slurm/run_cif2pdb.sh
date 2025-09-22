1406b1568fd1b8e62d5f05614cb7328a400cd1e6
wesleygomersall <gomersallw@gmail.com>
2025-09-22 15:57:13 -0700

    removed old file list

--- run_slurm/run_cif2pdb.sh
+++ run_slurm/run_cif2pdb.sh
@@ -13,10 +13,6 @@
 # Create list of files for .cif -> .pdf conversion: `cif2pdb.txt`
 FILE_LIST="cif2pdb.txt"
 
-# OLD, DELETE filenames is array of paths to .cif 
-# filenames=("/projects/parisahlab/wesg/af3_data/20250920_AF3_output/comdn230-c16-13/comdn230-c16-13_model.cif" #"/projects/parisahlab/wesg/af3_data/20250920_AF3_output/comdn230-c16-14/comdn230-c16-14_model.cif" #"/projects/parisahlab/wesg/af3_data/20250920_AF3_output/comdn230-c16-15/comdn230-c16-15_model.cif" #"/projects/parisahlab/wesg/af3_data/20250920_AF3_output/comdn230-c16-16/comdn230-c16-16_model.cif" #"/projects/parisahlab/wesg/af3_data/20250920_AF3_output/comdn230-c16-17/comdn230-c16-17_model.cif" #"/projects/parisahlab/wesg/af3_data/20250920_AF3_output/comdn230-c16-18/comdn230-c16-18_model.cif" #"/projects/parisahlab/wesg/af3_data/20250920_AF3_output/comdn230-c16/comdn230-c16_model.cif")
-
-
 if ! [ -s "$FILE_LIST" ]; then
     echo "$FILE_LIST is empty. Exiting."
     exit
