d6b30e3ae5fcb5414beaa3c55311040348c1993c
wesleygomersall <gomersallw@gmail.com>
2025-09-22 13:55:17 -0700

    pymol rather than pyrosetta for this

--- cif2pdb.py
+++ cif2pdb.py
@@ -1,31 +1,26 @@
 #!/usr/bin/env python3
 
 # Wesley Gomersall
-# Thu, 31 Jul 2025 17:51:27 -0700
+# Mon, 22 Sep 2025 13:52:49 -0700
 
 import argparse
-import pyrosetta
-from pyrosetta import dump_pdb
-import pyrosetta.rosetta.core.import_pose as import_pose
+import pymol2
 
 def cif_to_pdb(cifpath: str): 
     '''
-    Creates pdb file from pose read from cif file.
+    Creates pdb file from pose read from cif file. Requires PyMOL.
     Input:
         cifpath: str        Path to cif file.
     '''
-    pose = import_pose.pose_from_file(cifpath)
-    outputname = cifpath.strip(".cif") + ".pdb"
-    print(f"writing pdb to {outputname}")
-    pose.dump_pdb(outputname)
+    with pymol2.PyMOL() as pymol:
+        pymol.cmd.load(cifpath, 'mystructure')
+        pymol.cmd.save(cifpath.replace('.cif', '.pdb'), selection='mystructure')
     return
 
 def main():
     parser = argparse.ArgumentParser(description="Create pdb file from cif.")
     parser.add_argument("--input", "-i", type=str, help=".cif file path")
     args = parser.parse_args()
-
-    pyrosetta.init("-mute all")
     cif_to_pdb(args.input)
 
 if __name__=="__main__":
