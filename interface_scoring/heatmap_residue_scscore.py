633c20d987a6b931c6edccf3669071be6abe1eaa
Wesley Gomersall <gomersallw@gmail.com>
2025-12-08 16:43:23 -0800

    per res sc scoring

---
+++ interface_scoring/heatmap_residue_scscore.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import argparse 
+
+def main():
+
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--input", "-i", nargs='+', type=str, default = "", help="")
+    args = parser.parse_args()
+
+    packstats = pd.DataFrame()
+    for file in args.input: 
+        name = file.split('/')[-1].rstrip('_residuescscore.csv')
+        temp = pd.read_csv(file)
+        packstats[name] = temp.loc[:, 'sc_score']
+
+    plt.figure(figsize=(10, 5)) # Adjust figure size
+    sns.heatmap(packstats) 
+    plt.title("sc_score per residue")
+    plt.ylabel("Peptide residue number")
+    plt.subplots_adjust(bottom=0.4) 
+    plt.show()
+
+if __name__ == "__main__": 
+    main()
+
--- interface_scoring/sc_scoring.py
+++ interface_scoring/sc_scoring.py
@@ -4,6 +4,8 @@ import argparse
 import numpy as np
 import pyrosetta as pr
 from pyrosetta.rosetta.protocols.analysis import InterfaceAnalyzerMover
+from pyrosetta.rosetta.protocols.simple_filters import ShapeComplementarityFilter
+from pyrosetta.rosetta.core.select.residue_selector import ResidueIndexSelector
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("--file-list", "-f", type=str, help="Input pdb file list for interface scoring. One pdb path per line :-).")
@@ -11,6 +13,12 @@ parser.add_argument("--debug", action="store_true", default=False, help="Debug w
 args = parser.parse_args()
 
 pr.init()
+
+def create_index_selector(res_nums):
+    idx_selector = ResidueIndexSelector()
+    for res in res_nums:
+        idx_selector.append_index(res)
+    return idx_selector
 
 def score_interface(pose, interface ):
     # analyze interface statistics
@@ -45,16 +53,42 @@ def main():
     # get list of inputs
     my_file_list = []
     with open(args.file_list, 'r') as fin: 
-        line = fin.read().strip()
-        if line != '': my_file_list.append(line)
+        for line in fin:
+            file = line.strip()
+            if file != '': my_file_list.append(file)
 
     for pdb_file in my_file_list: 
-        pose = pr.pose_from_pdb(pdb_file)
+        with open(f"{pdb_file.rstrip('.pdb')}_residuescscore.csv", 'w') as fout:
+            fout.write("Residue,sc_score\n")
+            pose = pr.pose_from_pdb(pdb_file)
+            
+            binder_resis_beg = pose.conformation().chain_begin(2)
+            binder_resis_end = pose.conformation().chain_end(2)
 
-        myscores_all = score_interface(pose, "A_B")
+            for res in range(binder_resis_beg, binder_resis_end + 1):
+                print(res)
 
-        print(f"file: {pdb_file}")
-        print(f"interface sc_value: {myscores_all.sc_value}")
+                other_residues = list(range(1, pose.total_residue() + 1))
+                other_residues.remove(int(res))
+
+                if res < pose.total_residue() - 1:
+                    sel2_string = f"{min(other_residues)}-{res-1},{res+1}-{pose.total_residue()}"
+                elif res == pose.total_residue() - 1: 
+                    sel2_string = f"{min(other_residues)}-{res-1},{pose.total_residue()}"
+                elif res == pose.total_residue(): 
+                    sel2_string = f"{min(other_residues)}-{res-1}"
+                else: 
+                    sel2_string = f"{min(other_residues)}-{pose.total_residue()}"
+
+                sc = ShapeComplementarityFilter()
+                sc.use_rosetta_radii(True)
+                sc.residues1(str(res))
+                sc.residues2(sel2_string) 
+                sc_val = sc.score(pose)
+
+                val = sc_val
+                # val = myscores_all.sc_value
+                fout.write(f"{res},{val}\n")
 
 if __name__ == "__main__":
     main()
